4086094eebab232ff0d27b165a82ca7f02d3096c
Prasoon Singh <42083849+prasoons075@users.noreply.github.com>
2023-09-12 10:27:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # LinkedIn login credentials
 email = "rowdyme234@gmail.com"
-password = "*EB3vP9YfKUy!8"
+password = "*******"
 
 # input excel file with companies details
 data = "Linkedin_URLs_Company_List_POC.xlsx"
